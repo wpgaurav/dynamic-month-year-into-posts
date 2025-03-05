566c3b5d9c09d05a187422c14a9be51086bbfffb
meteorlxy <meteor.lxy@foxmail.com>
2025-03-05 22:23:58 +0800

    feat: avoid shortcodes being stripped

--- dynamic-month-year-into-posts.php
+++ dynamic-month-year-into-posts.php
@@ -278,6 +278,46 @@ function dmyip_enable_shortcode_in_archive_titles( $title ) {
 }
 add_filter( 'get_the_archive_title', 'dmyip_enable_shortcode_in_archive_titles' );
 
+// Avoid shortcodes being stripped
+add_filter( 'strip_shortcodes_tagnames', function ( $tags_to_remove ) {
+    return array_diff(
+        $tags_to_remove,
+        array(
+            'year',
+            'month',
+            'cmonth',
+            'mon',
+            'cmon',
+            'mm',
+            'mn',
+            'nmonth',
+            'cnmonth',
+            'pmonth',
+            'cpmonth',
+            'nmon',
+            'cnmon',
+            'pmon',
+            'cpmon',
+            'date',
+            'monthyear',
+            'nmonthyear',
+            'pmonthyear',
+            'nyear',
+            'nnyear',
+            'pyear',
+            'ppyear',
+            'dt',
+            'nd',
+            'pd',
+            'weekday',
+            'wd',
+            'blackfriday',
+            'cybermonday',
+            'datepublished',
+            'datemodified',
+        )
+    );
+});
 
 // Rank Math Support
 add_filter( 'rank_math/frontend/title', function( $title ) {
